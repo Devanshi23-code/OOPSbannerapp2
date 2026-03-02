56d8b0fc4ec2fa33b4d1b0d9fd391a090048741b
Devanshi23-code <devanshiagarwal052006@gmail.com>
2026-03-03 00:43:57 +0530

    updating

--- uc2.java
+++ uc2.java
@@ -1,13 +1,13 @@
 public class uc2 {
     public static void main()
     {
-        System.out.println("   ***   "+" "+"  *****  ");
-        System.out.println(" **   ** "+" "+" **      ");
-        System.out.println("**     **"+" "+"**       ");
-        System.out.println("**     **"+" "+"  *****  ");
-        System.out.println("**     **"+" "+"      ** ");
-        System.out.println(" **   ** "+" "+" **   ** ");
-        System.out.println("   ***   "+" "+"  *****  ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"  *****  ");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" **      ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
+        System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "+"  *****  ");
+        System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"      ** ");
+        System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+" **   ** ");
+        System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+"  *****  ");
 
     }
 }
